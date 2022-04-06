dd2f1911847f33f80f2558e485e5d3d718ea9c97
Aleksadra Pacia <olusiaOP05@gmail.com>
2022-04-06 20:27:01 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
+from kivy.properties import ObjectProperty
+
+
+class NavContent(BoxLayout):
+    management = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+
+class Demo(ScreenManager):
+    pass
+
+class UnknownApp(MDApp):
+    def build(self):
+        Builder.load_file('demo.kv')
+        return Demo()
+
+UnknownApp().run()

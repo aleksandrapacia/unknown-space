a51de9eea87962dbe2a577a631754490fd645ee6
Aleksadra Pacia <olusiaOP05@gmail.com>
2022-04-10 13:44:54 +0200

    screen manager outline

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty, VariableListProperty
 from kivy.uix.floatlayout import FloatLayout
 
@@ -11,14 +11,35 @@ class NavContent(BoxLayout):
     management = ObjectProperty()
     nav_drawer = ObjectProperty()
 
-
-class Demo(ScreenManager):
+class FirstScreen(Screen):
     pass
+
+class ProfileScreen(Screen):
+    pass
+
+class CalendarScreen(Screen):
+    pass
+
+class ArticlesScreen(Screen):
+    pass
+
+class SkyviewScreen(Screen):
+    pass
+
+
+class MyManager(ScreenManager):
+    pass
+
+    def build(self):
+        mm = MyManager()
+
+
+    
 
 class UnknownApp(MDApp):
     def build(self):
         Builder.load_file('demo.kv')
-        return Demo()
+        return MyManager()
 
 UnknownApp().run()
 
